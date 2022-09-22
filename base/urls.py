606ce17f93a79781ab2eeaa34efadbfecfccb492
ziyaoze <95531839+ziyaoze@users.noreply.github.com>
2022-09-22 11:26:44 +0500

    Update urls.py

--- base/urls.py
+++ base/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('publishers', views.publishers, name='publishers')
+    path('publishers', views.publishers, name='publishers'),
+    path('advertisers', views.advertisers, name='advertisers')
 
 ]
