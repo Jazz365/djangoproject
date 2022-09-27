c88eb2de6bbed506f7c9118843e5b701d2352708
ziyaoze <95531839+ziyaoze@users.noreply.github.com>
2022-09-27 19:50:07 +0500

    Update admin.py

--- base/admin.py
+++ base/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Home,SavedEmail, Publisher, Advertiser
+from .models import Home,SavedEmail, Publisher, Advertiser, Company
 # Register your models here.
 admin.site.register(Home)
 admin.site.register(SavedEmail)
 admin.site.register(Publisher)
 admin.site.register(Advertiser)
+
+admin.site.register(Company)
