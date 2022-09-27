e043a599ceecb978069105cfec3eb84c5cf476ab
ziyaoze <95531839+ziyaoze@users.noreply.github.com>
2022-09-27 19:29:15 +0500

    Update urls.py

--- base/urls.py
+++ base/urls.py
@@ -6,6 +6,12 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('publishers', views.publishers, name='publishers'),
-    path('advertisers', views.advertisers, name='advertisers')
+    path('advertisers', views.advertisers, name='advertisers'),
+    path('traffic', views.traffic, name='traffic'),
+    path('company', views.company, name='company'),
+    path('contact-us', views.contact, name='contact'),
+    path('signup', views.signup, name='signup'),
+    path('login', views.login, name='login'),
+
 
 ]
