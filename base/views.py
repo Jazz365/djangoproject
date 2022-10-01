bef05045df59deb0ddc428e784a6dfa9aa00db41
ziyaoze <95531839+ziyaoze@users.noreply.github.com>
2022-10-01 19:57:43 +0500

    Update views.py

--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Home, SavedEmail, Publisher, Advertiser, Company, TrafficPage, ContactPage, LoginPage, SignUpPage
-
+import requests
 
 # Create your views here.
 
